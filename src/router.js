b558f18f2f7abc58667d4e2393ac66a618fc8a9a
Steffen Dyhr-Nielsen <sdn@au2mate.local>
2019-07-10 15:29:45 +0200

    Initial commit

---
+++ src/router.js
@@ -0,0 +1,36 @@
+import Vue from "vue";
+import Router from "vue-router";
+import EventList from "./views/EventList.vue";
+import EventShow from "./views/EventShow.vue";
+import EventCreate from "./views/EventCreate.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      name: "event-list",
+      component: EventList
+    },
+    {
+      path: "/event",
+      name: "event-show",
+      component: EventShow
+    },
+    {
+      path: "/event/create",
+      name: "event-create",
+      component: EventCreate
+    }
+    // {
+    //   path: "/about",
+    //   name: "about",
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () =>
+    //     import(/* webpackChunkName: "about" */ "./views/About.vue")
+    // }
+  ]
+});
---
+++ src/views/EventCreate.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Event Create</h1>
+  </div>
+</template>
