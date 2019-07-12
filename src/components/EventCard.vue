732f7824b119a792e10cbf4b934c6df590528127
Steffen Dyhr-Nielsen <sdn@au2mate.local>
2019-07-12 08:28:56 +0200

    pre mastering vuex

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,17 +1,42 @@
 <template>
-	<div>
-		<h4></h4>
+	<div class="event-card -shadow">
+		<router-link
+			class="event-link"
+			:to="{ name: 'event-show', params: { id: event.id } }"
+		>
+			<span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+			<h4 class="title">{{ event.title }}</h4>
+			<BaseIcon name="users">{{ event.attendees.length }} attending</BaseIcon>
+		</router-link>
 	</div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			title: 'Park Cleanup'
-		}
+	props: {
+		event: Object
 	}
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.event-card {
+	padding: 20px;
+	margin-bottom: 24px;
+	transition: all 0.2s linear;
+	cursor: pointer;
+}
+.event-card:hover {
+	transform: scale(1.01);
+	box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+	margin: 0;
+}
+
+.event-link {
+	color: black;
+	text-decoration: none;
+	font-weight: 100;
+}
+</style>
