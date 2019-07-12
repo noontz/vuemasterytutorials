f7a81717853bac2da195d3c90b694c4125600bb8
Steffen Dyhr-Nielsen <sdn@au2mate.local>
2019-07-12 15:50:03 +0200

    actions part one

--- src/store.js
+++ src/store.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import EventService from '@/services/EventService.js'
 Vue.use(Vuex)
-
 export default new Vuex.Store({
 	state: {
 		user: { id: 'abc123', name: 'Adam Jahr' },
@@ -28,8 +27,18 @@ export default new Vuex.Store({
 			{ id: 4, title: '...', organizer: '...' }
 		]
 	},
-	mutations: {},
-	actions: {},
+	mutations: {
+		ADD_EVENT(state, event) {
+			state.events.push(event)
+		}
+	},
+	actions: {
+		createEvent({ commit }, event) {
+			return EventService.postEvent(event).then(() => {
+				commit('ADD_EVENT', event)
+			})
+		}
+	},
 	getters: {
 		catLength: state => {
 			return state.categories.length
---
+++ src/views/EventCreate_getters.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<h1>Create an Event, {{ $store.state.user.name }}</h1>
+		<p>This event is created by {{ userName }} id: {{ user.id }}</p>
+		<p>There are {{ catLength }} categories</p>
+		<ul>
+			<li v-for="cat in categories" :key="cat">{{ cat }}</li>
+		</ul>
+		<p>Finished todos</p>
+		<ul>
+			<li v-for="todo in doneTodos" :key="todo.id">{{ todo }}</li>
+		</ul>
+		<p>{{ getEventById(1) }}</p>
+	</div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+export default {
+	computed: {
+		catLength() {
+			return this.$store.getters.catLength
+		},
+		...mapState({
+			userName: state => state.user.name,
+			user: 'user',
+			categories: 'categories'
+		}),
+		...mapGetters(['getEventById', 'doneTodos'])
+	}
+}
+</script>
+
+<style lang="scss" scoped></style>
